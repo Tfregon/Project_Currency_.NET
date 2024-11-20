e4fbe82a7d85f045da1698bf07ec0013bd7de136
Thomas Edson Fregoneze <thomfregon@gmail.com>
2024-11-20 00:00:53 -0500

    First Commit

---
+++ CurrencyApp/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Python.Runtime;
+
+namespace CurrencyApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();          
+        }
+                
+        private void buttonBTCBRL_Click(object sender, EventArgs e)
+        {
+            // Carrega as informações da cotação
+            string resultBTCBRL = BTCBRL.ObterCotacaoBTCBRL();
+            // Exibe na label
+            labelResult.Text = resultBTCBRL;
+        }
+
+        private void buttonEURBRL_Click(object sender, EventArgs e)
+        {
+            // Carrega as informações da cotação
+            string resultEURBRL = EURBRL.ObterCotacaoEURBRL();
+            // Exibe na label
+            labelResult.Text = resultEURBRL;
+        }
+
+        private void buttonUSDBRL_Click(object sender, EventArgs e)
+        {
+            // Carrega as informações da cotação
+            string resultUSDBRL = USDBRL.ObterCotacaoUSDBRL();
+            // Exibe na label
+            labelResult.Text = resultUSDBRL;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
---
+++ CurrencyApp/OptionsOfCurrency.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CurrencyApp
+{
+    internal class BTCBRL
+    {
+        public static string ObterCotacaoBTCBRL()
+        {
+            ProcessStartInfo start = new ProcessStartInfo
+            {
+                FileName = @"C:\Users\Thomas\AppData\Local\Programs\Python\Python312\python.exe", // Caminho do executável Python
+                Arguments = @"C:\Users\Thomas\Documents\GitHub\ProjectApiCotacao\BTCBRL.py", // Caminho completo do script BTCBRL.py
+                RedirectStandardOutput = true,
+                UseShellExecute = false,
+                CreateNoWindow = true
+            };
+
+            using (Process process = Process.Start(start))
+            using (System.IO.StreamReader reader = process.StandardOutput)
+            {
+                return reader.ReadToEnd().Trim();
+            }
+        }
+    }
+
+    internal class EURBRL
+    {
+        public static string ObterCotacaoEURBRL()
+        {
+            ProcessStartInfo start = new ProcessStartInfo
+            {
+                FileName = @"C:\Users\Thomas\AppData\Local\Programs\Python\Python312\python.exe", // Caminho do executável Python
+                Arguments = @"C:\Users\Thomas\Documents\GitHub\ProjectApiCotacao\EURBRL.py", // Caminho completo do script BTCBRL.py
+                RedirectStandardOutput = true,
+                UseShellExecute = false,
+                CreateNoWindow = true
+            };
+
+            using (Process process = Process.Start(start))
+            using (System.IO.StreamReader reader = process.StandardOutput)
+            {
+                return reader.ReadToEnd().Trim();
+            }
+        }
+    }
+    internal class USDBRL
+    {
+        public static string ObterCotacaoUSDBRL()
+        {
+            ProcessStartInfo start = new ProcessStartInfo
+            {
+                FileName = @"C:\Users\Thomas\AppData\Local\Programs\Python\Python312\python.exe", // Caminho do executável Python
+                Arguments = @"C:\Users\Thomas\Documents\GitHub\ProjectApiCotacao\USDBRL.py", // Caminho completo do script BTCBRL.py
+                RedirectStandardOutput = true,
+                UseShellExecute = false,
+                CreateNoWindow = true
+            };
+
+            using (Process process = Process.Start(start))
+            using (System.IO.StreamReader reader = process.StandardOutput)
+            {
+                return reader.ReadToEnd().Trim();
+            }
+        }
+    }
+}
